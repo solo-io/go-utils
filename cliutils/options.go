9c78cd618bbc4ae42264e9218967eae63771a32d
EItanya <eitan.yarmush@solo.io>
2019-01-07 14:02:55 -0500

    cli options func

--- cliutils/options.go
+++ cliutils/options.go
@@ -2,9 +2,4 @@ package cliutils
 
 import "github.com/spf13/cobra"
 
-type Options interface {
-	Initialize() error
-}
-
-type OptionsFunc func(*cobra.Command) error
-type CmdFunc func(*Options, ...OptionsFunc) *cobra.Command
+type OptionsFunc func(*cobra.Command)
