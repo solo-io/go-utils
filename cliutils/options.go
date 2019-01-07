079c508db71832a2d5e484b563d612d23e186d9a
EItanya <eitan.yarmush@solo.io>
2019-01-07 14:14:31 -0500

    applied

--- cliutils/options.go
+++ cliutils/options.go
@@ -3,3 +3,9 @@ package cliutils
 import "github.com/spf13/cobra"
 
 type OptionsFunc func(*cobra.Command)
+
+func ApplyOptions(cmd *cobra.Command, funcs []OptionsFunc) {
+	for _, v := range funcs {
+		v(cmd)
+	}
+}
