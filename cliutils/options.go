2833c9d7db939a2fe0a64a425b2d97072854c10b
EItanya <eitan.yarmush@solo.io>
2019-01-08 11:39:32 -0500

    some extra cliutils

--- cliutils/options.go
+++ cliutils/options.go
@@ -1,11 +1,38 @@
 package cliutils
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"log"
+)
 
-type OptionsFunc func(*cobra.Command)
+type Options interface {
+	Initialize()
+}
 
-func ApplyOptions(cmd *cobra.Command, funcs []OptionsFunc) {
+type OptionsFunc = func(*cobra.Command)
+type CmdFunc = func(Options,  ...OptionsFunc) *cobra.Command
+
+func ApplyOptions(cmd *cobra.Command, funcs... OptionsFunc) {
 	for _, v := range funcs {
 		v(cmd)
+	}
+}
+
+func ReplaceCmd(parent *cobra.Command, new *cobra.Command) error {
+	parentCmds := parent.Commands()
+	for _, old := range parentCmds {
+		if old.Use == new.Use {
+			parent.RemoveCommand(old)
+			parent.AddCommand(new)
+			return nil
+		}
+	}
+	return fmt.Errorf("did not find child command to replace")
+}
+
+func MustReplaceCmd(parent *cobra.Command, new *cobra.Command) {
+	if err := ReplaceCmd(parent, new); err != nil {
+		log.Fatal(err)
 	}
 }
