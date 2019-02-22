2d473c05beff10be3e5debd7b33f3da394b3f029
EItanya <eitan.yarmush@solo.io>
2019-02-22 10:00:45 -0500

    ported basic docker commands

--- docker/exec.go
+++ docker/exec.go
@@ -1,42 +1,31 @@
-
-
 package docker
 
 import (
+	"fmt"
+	"go.uber.org/zap"
 	"io"
-
-	"sigs.k8s.io/kind/pkg/exec"
+	"os"
+	"os/exec"
 )
 
-type Cmder interface {
-	// command, args..., just like os/exec.Cmd
-	Command(string, ...string) Cmd
-}
 
-// Cmd abstracts over running a command somewhere, this is useful for testing
-type Cmd interface {
-	Run() error
-	// Each entry should be of the form "key=value"
-	SetEnv(...string)
-	SetStdin(io.Reader)
-	SetStdout(io.Writer)
-	SetStderr(io.Writer)
-}
-// containerCmder implements exec.Cmder for docker containers
-type containerCmder struct {
-	nameOrID string
-}
+var (
+	logger *zap.SugaredLogger
+)
 
-// ContainerCmder creates a new exec.Cmder against a docker container
-func ContainerCmder(containerNameOrID string) exec.Cmder {
-	return &containerCmder{
-		nameOrID: containerNameOrID,
+func init() {
+	var err error
+	devLogger, err := zap.NewDevelopment()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+	logger = devLogger.Sugar()
 }
 
-func (c *containerCmder) Command(command string, args ...string) exec.Cmd {
+
+func Command(command string, args ...string) *containerCmd {
 	return &containerCmd{
-		nameOrID: c.nameOrID,
 		command:  command,
 		args:     args,
 	}
@@ -54,64 +43,27 @@ type containerCmd struct {
 }
 
 func (c *containerCmd) Run() error {
-	args := []string{
-		"exec",
-		// run with priviliges so we can remount etc..
-		// this might not make sense in the most general sense, but it is
-		// important to many kind commands
-		"--privileged",
-	}
-	if c.stdin != nil {
-		args = append(args,
-			"-i", // interactive so we can supply input
-		)
-	}
-	if c.stderr != nil || c.stdout != nil {
-		args = append(args,
-			"-t", // use a tty so we can get output
-		)
-	}
+	var args []string
+
 	// set env
 	for _, env := range c.env {
 		args = append(args, "-e", env)
 	}
-	// specify the container and command, after this everything will be
-	// args the the command in the container rather than to docker
-	args = append(
-		args,
-		c.nameOrID, // ... against the container
-		c.command,  // with the command specified
-	)
 	args = append(
 		args,
 		// finally, with the caller args
 		c.args...,
 	)
+
 	cmd := exec.Command("docker", args...)
 	if c.stdin != nil {
-		cmd.SetStdin(c.stdin)
+		cmd.Stdin = c.stdin
 	}
 	if c.stderr != nil {
-		cmd.SetStderr(c.stderr)
+		cmd.Stderr = c.stderr
 	}
 	if c.stdout != nil {
-		cmd.SetStdout(c.stdout)
+		cmd.Stdout = c.stdout
 	}
 	return cmd.Run()
-}
-
-func (c *containerCmd) SetEnv(env ...string) {
-	c.env = env
-}
-
-func (c *containerCmd) SetStdin(r io.Reader) {
-	c.stdin = r
-}
-
-func (c *containerCmd) SetStdout(w io.Writer) {
-	c.stdout = w
-}
-
-func (c *containerCmd) SetStderr(w io.Writer) {
-	c.stderr = w
 }
--- docker/pull.go
+++ docker/pull.go
@@ -1,24 +1,18 @@
-
 package docker
 
 import (
+	"github.com/solo-io/go-utils/errors"
 	"time"
-
-	log "github.com/sirupsen/logrus"
-
-	"sigs.k8s.io/kind/pkg/exec"
 )
 
 // PullIfNotPresent will pull an image if it is not present locally
 // retrying up to retries times
 // it returns true if it attempted to pull, and any errors from pulling
 func PullIfNotPresent(image string, retries int) (pulled bool, err error) {
-	// TODO(bentheelder): switch most (all) of the logging here to debug level
-	// once we have configurable log levels
 	// if this did not return an error, then the image exists locally
-	cmd := exec.Command("docker", "inspect", "--type=image", image)
+	cmd := Command("docker", "inspect", "--type=image", image)
 	if err := cmd.Run(); err == nil {
-		log.Infof("Image: %s present locally", image)
+		logger.Infof("Image: %s present locally", image)
 		return false, nil
 	}
 	// otherwise try to pull it
@@ -27,22 +21,21 @@ func PullIfNotPresent(image string, retries int) (pulled bool, err error) {
 
 // Pull pulls an image, retrying up to retries times
 func Pull(image string, retries int) error {
-	log.Infof("Pulling image: %s ...", image)
-	err := exec.Command("docker", "pull", image).Run()
+	logger.Infof("Pulling image: %s ...", image)
+	err := Command("docker", "pull", image).Run()
 	// retry pulling up to retries times if necessary
 	if err != nil {
 		for i := 0; i < retries; i++ {
 			time.Sleep(time.Second * time.Duration(i+1))
-			log.WithError(err).Infof("Trying again to pull image: %s ...", image)
-			// TODO(bentheelder): add some backoff / sleep?
-			err = exec.Command("docker", "pull", image).Run()
+			logger.Warnf(errors.Wrapf(err, "Trying again to pull image: %s ...", image).Error())
+			err = Command("docker", "pull", image).Run()
 			if err == nil {
 				break
 			}
 		}
 	}
 	if err != nil {
-		log.WithError(err).Infof("Failed to pull image: %s", image)
+		logger.Warnf(errors.Wrapf(err, "Failed to pull image: %s", image).Error())
 	}
 	return err
 }
