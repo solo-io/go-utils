de1ab502dcf4018a37ce21e1e79203a12cbe7e71
EItanya <eitan.yarmush@solo.io>
2019-02-26 09:29:29 -0500

    removed github changelog code

--- changelogutils/changelog.go
+++ changelogutils/changelog.go
@@ -3,17 +3,13 @@ package changelogutils
 import (
 	"context"
 	"github.com/ghodss/yaml"
+	"github.com/pkg/errors"
 	"github.com/solo-io/go-utils/githubutils"
+	"github.com/solo-io/go-utils/versionutils"
+	"io/ioutil"
+	"path/filepath"
 )
 
-type ChangelogEntryType int
-
-const (
-	BREAKING_CHANGE ChangelogEntryType = iota
-	FIX
-	NEW_FEATURE
-	NON_USER_FACING
-)
 
 type ChangelogEntry struct {
 	Type        ChangelogEntryType
@@ -50,19 +46,50 @@ func GetLatestTag(ctx context.Context, owner, repo string) (string, error) {
 	return githubutils.FindLatestReleaseTag(ctx, client, owner, repo)
 }
 
-
-
-func BytesToChangelog(rcl []*RawChangelogFile) (*Changelog, error) {
-	cl := &Changelog{
-		Files: make([]ChangelogFile, len(rcl)),
+// Should return the next version to release, based on the names of the subdirectories in the changelog
+// Will return an error if there is no version, or multiple versions, larger than the latest tag,
+// according to semver
+func GetProposedTagLocal(latestTag, changelogParentPath string) (string, error) {
+	changelogPath := filepath.Join(changelogParentPath, ChangelogDirectory)
+	subDirs, err := ioutil.ReadDir(changelogPath)
+	if err != nil {
+		return "", errors.Wrapf(err, "Error reading changelog directory")
 	}
-	for _, file := range rcl {
-		var clf ChangelogFile
-		err := yaml.Unmarshal(file.Bytes, &clf)
-		if err != nil {
-			return nil, err
+	proposedVersion := ""
+	for _, subDir := range subDirs {
+		if !subDir.IsDir() {
+			return "", errors.Errorf("Unexpected entry %s in changelog directory", subDir.Name())
 		}
-		cl.Files = append(cl.Files, clf)
+		if !versionutils.MatchesRegex(subDir.Name()) {
+			return "", errors.Errorf("Directory name %s is not valid, must be of the form 'vX.Y.Z'", subDir.Name())
+		}
+		greaterThan, err := versionutils.IsGreaterThanTag(subDir.Name(), latestTag)
+		if err != nil {
+			return "", err
+		}
+		if greaterThan {
+			if proposedVersion != "" {
+				return "", errors.Errorf("Versions %s and %s are both greater than latest tag", subDir.Name(), proposedVersion)
+			}
+			proposedVersion = subDir.Name()
+		}
 	}
-	return cl, nil
+	if proposedVersion == "" {
+		return "",  errors.Errorf("No version greater than %s found", latestTag)
+	}
+	return proposedVersion, nil
+}
+
+func ReadChangelogFile(path string) (*Changelog, error) {
+	var changelog Changelog
+	bytes, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed reading changelog file: %s", path)
+	}
+
+	if err := yaml.Unmarshal(bytes, changelog); err != nil {
+		return nil, errors.Wrap(err, "failed parsing changelog file")
+	}
+
+	return &changelog, nil
 }
--- changelogutils/changelog_test.go
+++ changelogutils/changelog_test.go
@@ -1,11 +1,8 @@
 package changelogutils
 
 import (
-	"context"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"github.com/solo-io/go-utils/githubutils"
-
 	"github.com/solo-io/solo-kit/pkg/utils/log"
 	"io/ioutil"
 	"os"
@@ -31,16 +28,6 @@ var _ = Describe("ChangelogTest", func() {
 		Expect(os.Mkdir(changelogDir, 0700)).To(BeNil())
 		Expect(createSubdirs(changelogDir, "v0.0.1", "v0.0.2", "v0.0.3", "v0.0.4")).To(BeNil())
 		expectGetProposedTag("v0.0.3", tmpDir, "v0.0.4", "")
-	})
-
-	It("works with git", func() {
-		ctx := context.Background()
-		client, err := githubutils.GetClient(ctx)
-		Expect(err).NotTo(HaveOccurred())
-		_, err = GetProposedTagFromGit(ctx, client, "solo-io", "testrepo", "v0.0.16")
-		Expect(err).NotTo(HaveOccurred())
-		_, err = ValidateProposedChangelogTag(ctx, client, "solo-io", "testrepo", "v0.0.16")
-		Expect(err).NotTo(HaveOccurred())
 	})
 })
 
