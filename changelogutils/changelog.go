e4e90e19e54e30cf533ecf41beacfb602d0fe84a
EItanya <eitan.yarmush@solo.io>
2019-02-27 09:56:32 -0500

    minor error changes

--- changelogutils/changelog.go
+++ changelogutils/changelog.go
@@ -95,7 +95,7 @@ func ReadChangelogFile(fs afero.Fs, path string) (*ChangelogFile, error) {
 	}
 
 	if err := yaml.Unmarshal(bytes, &changelog); err != nil {
-		return nil, errors.Errorf("File %s is not a valid changelog file", path)
+		return nil, errors.Errorf("File %s is not a valid changelog file. Error: %v", path, err)
 	}
 
 	for _, entry := range changelog.Entries {
