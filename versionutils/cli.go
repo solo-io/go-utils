af7928af104b1a8e1ad821ad8beea1e743eaf63d
Rick Ducott <rick.ducott@solo.io>
2019-03-07 07:47:32 -0500

    Fix message

--- versionutils/cli.go
+++ versionutils/cli.go
@@ -8,12 +8,12 @@ import (
 func GetReleaseVersionOrExitGracefully() *Version {
 	tag, present := os.LookupEnv("TAGGED_VERSION")
 	if !present || tag == "" {
-		fmt.Printf("TAGGED_VERSION not found in environment, skipping docs PR.\n")
+		fmt.Printf("TAGGED_VERSION not found in environment, exiting.\n")
 		os.Exit(0)
 	}
 	version, err := ParseVersion(tag)
 	if err != nil {
-		fmt.Printf("TAGGED_VERSION %s is not a valid semver version, skipping docs PR.\n", tag)
+		fmt.Printf("TAGGED_VERSION %s is not a valid semver version, exiting.\n", tag)
 		os.Exit(0)
 	}
 	return version
