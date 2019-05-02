4530b2a7654e0e75770cb8b33e394ff3b47473e8
EItanya <eitan.yarmush@solo.io>
2019-05-02 13:55:18 -0400

    remove ununsed code

--- installutils/kubeinstall/kubeinstall_suite_test.go
+++ installutils/kubeinstall/kubeinstall_suite_test.go
@@ -11,21 +11,3 @@ func TestKubeinstall(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Kubeinstall Suite")
 }
-
-// var (
-// 	lock *clusterlock.TestClusterLocker
-// 	err  error
-// )
-//
-// var _ = SynchronizedBeforeSuite(func() []byte {
-// 	lock, err = clusterlock.NewTestClusterLocker(kube.MustKubeClient(), clusterlock.Options{
-// 		IdPrefix: os.ExpandEnv("supergloo-helm-{$BUILD_ID}-"),
-// 	})
-// 	Expect(err).NotTo(HaveOccurred())
-// 	Expect(lock.AcquireLock()).NotTo(HaveOccurred())
-// 	return nil
-// }, func(data []byte) {})
-//
-// var _ = SynchronizedAfterSuite(func() {}, func() {
-// 	Expect(lock.ReleaseLock()).NotTo(HaveOccurred())
-// })
