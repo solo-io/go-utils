9567c61546c2289ae9459d48be4a2a44fe8a5811
EItanya <eitan.yarmush@solo.io>
2019-05-08 07:44:18 -0400

    fixed logic error

--- tarutils/main.go
+++ tarutils/main.go
@@ -27,6 +27,10 @@ func Tar(src string, fs afero.Fs, writers ...io.Writer) error {
 	defer tw.Close()
 	// walk path
 	return afero.Walk(fs, src, func(file string, fi os.FileInfo, err error) error {
+		if file == src {
+			return nil
+		}
+
 		filePrefix := getFilePrefix(srcInfo.IsDir(), src, file)
 		header, err := tar.FileInfoHeader(fi, fi.Name())
 		if err != nil {
@@ -60,8 +64,9 @@ func Tar(src string, fs afero.Fs, writers ...io.Writer) error {
 
 func getFilePrefix(srcDir bool, absoluteRoot, file string) string {
 	var relativePath string
-	if srcDir {
-		relativePath = strings.Replace(file, fmt.Sprintf("%s/", absoluteRoot), "", 1)
+	if srcDir && file != absoluteRoot {
+		pathToSrc := filepath.Dir(file)
+		relativePath = strings.Replace(pathToSrc, absoluteRoot, "", 1)
 	}
 	return relativePath
 }
