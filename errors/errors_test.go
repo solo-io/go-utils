328f801892d405e2124f00923fe386e67c9fe2f7
EItanya <eitan.yarmush@solo.io>
2019-05-16 14:14:30 -0400

    new error comparison with xerrors

---
+++ errors/errors_test.go
@@ -0,0 +1,19 @@
+package errors
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("new golang errors", func() {
+	var (
+		baseError = New("base error")
+		wrapperError1 = Wrapf(baseError, "wrapper 1")
+		wrapperError2 = Wrapf(wrapperError1, "wrapper 2")
+	)
+
+	It("can compare underlyin error values", func() {
+		Expect(Is(wrapperError1, baseError)).To(BeTrue())
+		Expect(Is(wrapperError2, baseError)).To(BeTrue())
+	})
+})
