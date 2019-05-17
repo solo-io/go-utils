7d29f8d63fc91d4a87fc6b03676a2f1e486301f9
EItanya <eitan.yarmush@solo.io>
2019-05-17 12:37:31 -0400

    termination-logs utils

---
+++ kubeutils/pod_termination.go
@@ -0,0 +1,29 @@
+package kubeutils
+
+import (
+	"os"
+
+	"github.com/solo-io/go-utils/log"
+)
+
+const (
+	TERMINATION_LOG = "/dev/termination-log"
+)
+
+/*
+This function is in kubeutils because it is meant to only be used in containers deployed in kubernetes
+
+Upon termination kubernetes pods will read from a log file in the container and output the contents to
+the pod spec for debugging. https://kubernetes.io/docs/tasks/debug-application-cluster/determine-reason-pod-failure/
+ */
+
+func LogFailureState(failureErr error) {
+	file, err := os.OpenFile(TERMINATION_LOG, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0777)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	_, err = file.Write([]byte(failureErr.Error()))
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+}
