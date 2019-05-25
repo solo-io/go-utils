1c4496595a794bad6cf47ad56f7d3e858403dce6
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-05-25 09:34:10 -0400

    dont create termination log

--- kubeutils/pod_termination.go
+++ kubeutils/pod_termination.go
@@ -15,10 +15,10 @@ This function is in kubeutils because it is meant to only be used in containers 
 
 Upon termination kubernetes pods will read from a log file in the container and output the contents to
 the pod spec for debugging. https://kubernetes.io/docs/tasks/debug-application-cluster/determine-reason-pod-failure/
- */
+*/
 
 func LogFailureState(failureErr error) {
-	file, err := os.OpenFile(TERMINATION_LOG, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0777)
+	file, err := os.Open(TERMINATION_LOG, os.O_RDWR, 0)
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
