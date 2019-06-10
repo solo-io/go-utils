d06d58efc041553763995c0f77617e2ed3817cf9
Rick Ducott <rick.ducott@solo.io>
2019-06-10 15:31:20 -0400

    Try refactoring to allow namespaced scopes

--- installutils/kuberesource/get_cluster_resources.go
+++ installutils/kuberesource/get_cluster_resources.go
@@ -14,31 +14,35 @@ import (
 	"k8s.io/client-go/rest"
 )
 
-type ClusterResourceFetcher struct {
+type clusterResourceFetcher struct {
+	cfg         *rest.Config
 	filterFuncs []FilterResource
+	namespaces  []string
+}
+
+func NewClusterResourceFetcher(cfg *rest.Config, namespaces []string, filterFuncs ...FilterResource) ClusterResourceFetcher {
+	return &clusterResourceFetcher{
+		cfg:         cfg,
+		namespaces:  namespaces,
+		filterFuncs: filterFuncs,
+	}
 }
 
 type FilterResource func(schema.GroupVersionResource) bool
 
-/*
-Use to get all CRUD'able resources for a cluster
-Returns a list of UnstructuredResources which are a wrapper for the
-map[string]interface{} type created from Kubernetes-style JSON objects.
+type ClusterResourceFetcher interface {
+	GetClusterResources(ctx context.Context) (UnstructuredResources, error)
+}
 
-Warning: slow function, be sure to call asynchronously.
-Filter funcs can be passed to reduce the latency of this function,
-i.e.: by excluding resource types (each resource type gets its own
-query, contributing to latency of this function).
-*/
-func GetClusterResources(ctx context.Context, cfg *rest.Config, filterFuncs ...FilterResource) (UnstructuredResources, error) {
+func (c *clusterResourceFetcher) GetClusterResources(ctx context.Context) (UnstructuredResources, error) {
 	// discovery client
-	disc, err := discovery.NewDiscoveryClientForConfig(cfg)
+	disc, err := discovery.NewDiscoveryClientForConfig(c.cfg)
 	if err != nil {
 		return nil, err
 	}
 
 	// resource client
-	client, err := dynamic.NewForConfig(cfg)
+	client, err := dynamic.NewForConfig(c.cfg)
 	if err != nil {
 		return nil, err
 	}
@@ -63,22 +67,40 @@ func GetClusterResources(ctx context.Context, cfg *rest.Config, filterFuncs ...F
 	var writeAccess sync.Mutex
 	var allResources UnstructuredResources
 	var g errgroup.Group
-	for _, gvr := range filterGroupVersions(groupVersionResources, filterFuncs...) {
+	for _, gvr := range filterGroupVersions(groupVersionResources, c.filterFuncs...) {
 		gvr := gvr
 		g.Go(func() error {
 			contextutils.LoggerFrom(ctx).Infow("listing all", "resourceType", gvr)
-			resources, err := client.Resource(gvr).List(metav1.ListOptions{})
-			if err != nil {
-				return emperror.With(err, "group_version_resource", gvr)
+
+			if c.namespaces == nil {
+				resources, err := client.Resource(gvr).List(metav1.ListOptions{})
+				if err != nil {
+					return emperror.With(err, "group_version_resource", gvr)
+				}
+
+				for i := range resources.Items {
+					res := &resources.Items[i]
+					contextutils.LoggerFrom(ctx).Debugf("appending %s: %v.%v", res.GroupVersionKind(), res.GetNamespace(), res.GetName())
+					writeAccess.Lock()
+					allResources = append(allResources, res)
+					writeAccess.Unlock()
+				}
+			} else {
+				for _, ns := range c.namespaces {
+					resources, err := client.Resource(gvr).Namespace(ns).List(metav1.ListOptions{})
+					if err != nil {
+						return emperror.With(err, "group_version_resource", gvr)
+					}
+					for i := range resources.Items {
+						res := &resources.Items[i]
+						contextutils.LoggerFrom(ctx).Debugf("appending %s: %v.%v", res.GroupVersionKind(), res.GetNamespace(), res.GetName())
+						writeAccess.Lock()
+						allResources = append(allResources, res)
+						writeAccess.Unlock()
+					}
+				}
 			}
 
-			for i := range resources.Items {
-				res := &resources.Items[i]
-				contextutils.LoggerFrom(ctx).Debugf("appending %s: %v.%v", res.GroupVersionKind(), res.GetNamespace(), res.GetName())
-				writeAccess.Lock()
-				allResources = append(allResources, res)
-				writeAccess.Unlock()
-			}
 			return nil
 		})
 	}
@@ -88,6 +110,20 @@ func GetClusterResources(ctx context.Context, cfg *rest.Config, filterFuncs ...F
 	}
 
 	return allResources.Sort(), nil
+}
+
+/*
+Use to get all CRUD'able resources for a cluster
+Returns a list of UnstructuredResources which are a wrapper for the
+map[string]interface{} type created from Kubernetes-style JSON objects.
+
+Warning: slow function, be sure to call asynchronously.
+Filter funcs can be passed to reduce the latency of this function,
+i.e.: by excluding resource types (each resource type gets its own
+query, contributing to latency of this function).
+*/
+func GetClusterResources(ctx context.Context, cfg *rest.Config, filterFuncs ...FilterResource) (UnstructuredResources, error) {
+	return NewClusterResourceFetcher(cfg, nil, filterFuncs...).GetClusterResources(ctx)
 }
 
 func filterGroupVersions(groupVersions []schema.GroupVersionResource, filterFuncs ...FilterResource) []schema.GroupVersionResource {
