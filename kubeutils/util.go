50ad03f8c85bb6007bc8f834464e5732d3eccd0f
Mitch Kelley <mitch.kelley@solo.io>
2019-07-11 16:37:18 -0400

    changelog and single quotes

--- kubeutils/util.go
+++ kubeutils/util.go
@@ -6,6 +6,8 @@ import (
 	"strings"
 )
 
+// use SanitizeNameV2
+// DEPRECATED
 func SanitizeName(name string) string {
 	name = strings.Replace(name, "*", "-", -1)
 	name = strings.Replace(name, "/", "-", -1)
@@ -15,6 +17,27 @@ func SanitizeName(name string) string {
 	name = strings.Replace(name, ":", "-", -1)
 	name = strings.Replace(name, " ", "-", -1)
 	name = strings.Replace(name, "\n", "", -1)
+	if len(name) > 63 {
+		hash := md5.Sum([]byte(name))
+		name = fmt.Sprintf("%s-%x", name[:31], hash)
+		name = name[:63]
+	}
+	name = strings.Replace(name, ".", "-", -1)
+	name = strings.ToLower(name)
+	return name
+}
+
+func SanitizeNameV2(name string) string {
+	name = strings.Replace(name, "*", "-", -1)
+	name = strings.Replace(name, "/", "-", -1)
+	name = strings.Replace(name, ".", "-", -1)
+	name = strings.Replace(name, "[", "", -1)
+	name = strings.Replace(name, "]", "", -1)
+	name = strings.Replace(name, ":", "-", -1)
+	name = strings.Replace(name, " ", "-", -1)
+	name = strings.Replace(name, "\n", "", -1)
+	name = strings.Replace(name, "\"", "", -1)
+	name = strings.Replace(name, "'", "", -1)
 	if len(name) > 63 {
 		hash := md5.Sum([]byte(name))
 		name = fmt.Sprintf("%s-%x", name[:31], hash)
