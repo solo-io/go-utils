fd8a77f0842ac1b8cb8307639d80944462691b24
Scott Weiss <sdw35@cornell.edu>
2019-08-21 22:23:04 -0400

    consul cluster locker

--- testutils/clusterlock/cluster_lock_client.go
+++ testutils/clusterlock/cluster_lock_client.go
@@ -1,6 +1,10 @@
 package clusterlock
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/consul/api"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -16,19 +20,16 @@ type ClusterLock struct {
 
 	// only for kube
 	ResourceVersion string
-	CfgMap          *v1.ConfigMap
 }
 
 func (l *ClusterLock) Empty() bool {
 	return l.OwnerID == "" && l.Timeout == ""
 }
 
-
 func (l *ClusterLock) Set(ownerId, timeout string) {
 	l.OwnerID = ownerId
 	l.Timeout = timeout
 }
-
 
 func (l ClusterLock) ConfigMap(namespace string) *v1.ConfigMap {
 	return &v1.ConfigMap{
@@ -51,14 +52,43 @@ func CLFromConfigMap(cm *v1.ConfigMap) *ClusterLock {
 	}
 
 	return &ClusterLock{
-		Name: cm.Name,
-		OwnerID:ownerId,
-		Timeout: timeout,
+		Name:            cm.Name,
+		OwnerID:         ownerId,
+		Timeout:         timeout,
 		ResourceVersion: cm.ResourceVersion,
-		CfgMap: cm,
 	}
 }
 
+var separator = "@"
+
+func toData(ownerId, timeout string) []byte {
+	return []byte(fmt.Sprintf("%v%v%v", ownerId, separator, timeout))
+}
+
+func fromData(data []byte) (string, string) {
+	parts := strings.Split(string(data), separator)
+	if len(parts) != 2 {
+		return "", ""
+	}
+	return parts[0], parts[1]
+}
+
+func (l ClusterLock) KVPair(keyPrefix string) *api.KVPair {
+	return &api.KVPair{
+		Key:   keyPrefix + l.Name,
+		Value: toData(l.OwnerID, l.Timeout),
+	}
+}
+
+func CLFromKVPair(keyPrefix string, kvp *api.KVPair) *ClusterLock {
+	ownerId, timeout := fromData(kvp.Value)
+
+	return &ClusterLock{
+		Name:    strings.TrimPrefix(kvp.Key, keyPrefix),
+		OwnerID: ownerId,
+		Timeout: timeout,
+	}
+}
 
 type ClusterLockClient interface {
 	Create(cl *ClusterLock) (*ClusterLock, error)
@@ -102,4 +132,38 @@ func (c *KubeClusterLockClient) Delete(name string) error {
 
 func NotFoundError(name string) error {
 	return errors.NewNotFound(v1.Resource("configmap"), name)
+}
+
+type ConsulClusterLockClient struct {
+	client    *api.Client
+	keyPrefix string
+}
+
+func (c *ConsulClusterLockClient) Create(cl *ClusterLock) (*ClusterLock, error) {
+	kvp := cl.KVPair(c.keyPrefix)
+	_, err := c.client.KV().Put(kvp, nil)
+	if err != nil {
+		return nil, err
+	}
+	return cl, nil
+}
+
+func (c *ConsulClusterLockClient) Update(cl *ClusterLock) (*ClusterLock, error) {
+	return c.Create(cl)
+}
+
+func (c *ConsulClusterLockClient) Get(name string) (*ClusterLock, error) {
+	kvp, _, err := c.client.KV().Get(c.keyPrefix+name, nil)
+	if err != nil {
+		return nil, err
+	}
+	if kvp == nil {
+		return nil, NotFoundError(name)
+	}
+	return CLFromKVPair(c.keyPrefix, kvp), nil
+}
+
+func (c *ConsulClusterLockClient) Delete(name string) error {
+	_, err := c.client.KV().Delete(c.keyPrefix+name, nil)
+	return err
 }
