b80d0d849770460e38021c812f025291d77db80d
Rick Ducott <rick.ducott@solo.io>
2020-01-23 13:25:18 -0500

    Updates

---
+++ networkutils/resource.go
@@ -0,0 +1,43 @@
+package networkutils
+
+import (
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// Get the resource identified by the given URI.
+// The URI can either be an http(s) address or a relative/absolute file path.
+func GetResource(uri string) (io.ReadCloser, error) {
+	var file io.ReadCloser
+	if strings.HasPrefix(uri, "http://") || strings.HasPrefix(uri, "https://") {
+		resp, err := http.Get(uri)
+		if err != nil {
+			return nil, err
+		}
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, errors.Errorf("http GET returned status %d for resource %s", resp.StatusCode, uri)
+		}
+
+		file = resp.Body
+	} else {
+		path, err := filepath.Abs(uri)
+		if err != nil {
+			return nil, errors.Wrapf(err, "getting absolute path for %v", uri)
+		}
+
+		f, err := os.Open(path)
+		if err != nil {
+			return nil, errors.Wrapf(err, "opening file %v", path)
+		}
+		file = f
+	}
+
+	// Write the body to file
+	return file, nil
+}
