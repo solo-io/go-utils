bc9398f8ecced443203a6cfa581285d8e9017902
EItanya <eitan.yarmush@solo.io>
2020-02-02 15:49:19 -0500

    formatting

---
+++ installutils/helminstall/client.go
@@ -0,0 +1,231 @@
+package helminstall
+
+import (
+	"io/ioutil"
+	"os"
+
+	"github.com/spf13/afero"
+	"helm.sh/helm/v3/pkg/action"
+	"helm.sh/helm/v3/pkg/chart"
+	"helm.sh/helm/v3/pkg/chart/loader"
+	"helm.sh/helm/v3/pkg/cli"
+	"helm.sh/helm/v3/pkg/release"
+)
+
+//go:generate mockgen -destination mocks/mock_helm_client.go -source ./client.go
+
+const (
+	tempChartFilePermissions = 0644
+	helmNamespaceEnvVar      = "HELM_NAMESPACE"
+)
+
+var verbose bool
+
+func setVerbose(b bool) {
+	verbose = b
+}
+
+// This interface implements the Helm CLI actions. The implementation relies on the Helm 3 libraries.
+type HelmClient interface {
+	// Prepare an installation object that can then be .Run() with a chart object
+	NewInstall(namespace, releaseName string, dryRun bool) (HelmInstall, *cli.EnvSettings, error)
+
+	// Prepare an un-installation object that can then be .Run() with a release name
+	NewUninstall(namespace string) (HelmUninstall, error)
+
+	// List the already-existing releases in the given namespace
+	ReleaseList(namespace string) (HelmReleaseListRunner, error)
+
+	// Returns the Helm chart archive located at the given URI (can be either an http(s) address or a file path)
+	DownloadChart(chartArchiveUri string) (*chart.Chart, error)
+
+	// Returns true if the release with the given name exists in the given namespace
+	ReleaseExists(namespace, releaseName string) (releaseExists bool, err error)
+}
+
+// slim interface on top of loader to avoid unecessary FS calls
+type HelmLoader interface {
+	Load(name string) (*chart.Chart, error)
+}
+
+// an interface around Helm's action.Install struct
+type HelmInstall interface {
+	Run(chrt *chart.Chart, vals map[string]interface{}) (*release.Release, error)
+}
+
+// an interface around Helm's action.Uninstall struct
+type HelmUninstall interface {
+	Run(name string) (*release.UninstallReleaseResponse, error)
+}
+
+var _ HelmInstall = &action.Install{}
+var _ HelmUninstall = &action.Uninstall{}
+
+// an interface around Helm's action.List struct
+type HelmReleaseListRunner interface {
+	Run() ([]*release.Release, error)
+	SetFilter(filter string)
+}
+
+type helmLoader struct{}
+
+func (h *helmLoader) Load(name string) (*chart.Chart, error) {
+	return loader.Load(name)
+}
+
+func NewHelmLoader() HelmLoader {
+	return &helmLoader{}
+}
+
+// a HelmClient that talks to the kube api server and creates resources
+func DefaultHelmClient() HelmClient {
+	return &defaultHelmClient{
+		fs:     afero.NewOsFs(),
+		loader: NewHelmLoader(),
+	}
+}
+
+type defaultHelmClient struct {
+	fs     afero.Fs
+	loader HelmLoader
+}
+
+func NewDefaultHelmClient(fs afero.Fs, loader HelmLoader) *defaultHelmClient {
+	return &defaultHelmClient{fs: fs, loader: loader}
+}
+
+func (d *defaultHelmClient) NewInstall(namespace, releaseName string, dryRun bool) (HelmInstall, *cli.EnvSettings, error) {
+	actionConfig, settings, err := newActionConfig(namespace)
+	if err != nil {
+		return nil, nil, err
+	}
+	settings.Debug = verbose
+
+	client := action.NewInstall(actionConfig)
+	client.ReleaseName = releaseName
+	client.Namespace = namespace
+	client.DryRun = dryRun
+
+	// If this is a dry run, we don't want to query the API server.
+	// In the future we can make this configurable to emulate the `helm template --validate` behavior.
+	client.ClientOnly = dryRun
+
+	return client, settings, nil
+}
+
+func (d *defaultHelmClient) NewUninstall(namespace string) (HelmUninstall, error) {
+	actionConfig, _, err := newActionConfig(namespace)
+	if err != nil {
+		return nil, err
+	}
+	return action.NewUninstall(actionConfig), nil
+}
+
+type helmReleaseListRunner struct {
+	list *action.List
+}
+
+func (h *helmReleaseListRunner) Run() ([]*release.Release, error) {
+	return h.list.Run()
+}
+
+func (h *helmReleaseListRunner) SetFilter(filter string) {
+	h.list.Filter = filter
+}
+
+func (d *defaultHelmClient) ReleaseList(namespace string) (HelmReleaseListRunner, error) {
+	actionConfig, _, err := newActionConfig(namespace)
+	if err != nil {
+		return nil, err
+	}
+	return &helmReleaseListRunner{
+		list: action.NewList(actionConfig),
+	}, nil
+}
+
+func (d *defaultHelmClient) DownloadChart(chartArchiveUri string) (*chart.Chart, error) {
+
+	// 1. Get a reader to the chart file (remote URL or local file path)
+	chartFileReader, err := GetResource(chartArchiveUri)
+	if err != nil {
+		return nil, err
+	}
+	defer func() { _ = chartFileReader.Close() }()
+
+	// 2. Write chart to a temporary file
+	chartBytes, err := ioutil.ReadAll(chartFileReader)
+	if err != nil {
+		return nil, err
+	}
+
+	chartFile, err := afero.TempFile(d.fs, "", "gloo-helm-chart")
+	if err != nil {
+		return nil, err
+	}
+	charFilePath := chartFile.Name()
+	defer func() { _ = d.fs.RemoveAll(charFilePath) }()
+
+	if err := afero.WriteFile(d.fs, charFilePath, chartBytes, tempChartFilePermissions); err != nil {
+		return nil, err
+	}
+
+	// 3. Load the chart file
+	chartObj, err := d.loader.Load(charFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	return chartObj, nil
+}
+
+func (d *defaultHelmClient) ReleaseExists(namespace, releaseName string) (releaseExists bool, err error) {
+	list, err := d.ReleaseList(namespace)
+	if err != nil {
+		return false, err
+	}
+	list.SetFilter(releaseName)
+
+	releases, err := list.Run()
+	if err != nil {
+		return false, err
+	}
+
+	for _, r := range releases {
+		releaseExists = releaseExists || r.Name == releaseName
+	}
+
+	return releaseExists, nil
+}
+
+// Build a Helm EnvSettings struct
+// basically, abstracted cli.New() into our own function call because of the weirdness described in the big comment below
+func NewCLISettings(namespace string) *cli.EnvSettings {
+	// The installation namespace is expressed as a "config override" in the Helm internals
+	// It's normally set by the --namespace flag when invoking the Helm binary, which ends up
+	// setting a non-exported field in the Helm settings struct (https://github.com/helm/helm/blob/v3.0.1/pkg/cli/environment.go#L77)
+	// However, we are not invoking the Helm binary, so that field doesn't get set. It is left as "", which means
+	// that any resources that are non-namespaced (at the time of writing, some of Prometheus's resources do not
+	// have a namespace attached to them but they probably should) wind up in the default namespace from YOUR
+	// kube config. To get around this, we temporarily set an env var before the Helm settings are initialized
+	// so that the proper namespace override is piped through. (https://github.com/helm/helm/blob/v3.0.1/pkg/cli/environment.go#L64)
+	if os.Getenv(helmNamespaceEnvVar) == "" {
+		os.Setenv(helmNamespaceEnvVar, namespace)
+		defer os.Setenv(helmNamespaceEnvVar, "")
+	}
+
+	return cli.New()
+}
+
+func noOpDebugLog(_ string, _ ...interface{}) {}
+
+// Returns an action configuration that can be used to create Helm actions and the Helm env settings.
+// We currently get the Helm storage driver from the standard HELM_DRIVER env (defaults to 'secret').
+func newActionConfig(namespace string) (*action.Configuration, *cli.EnvSettings, error) {
+	settings := NewCLISettings(namespace)
+	actionConfig := new(action.Configuration)
+
+	if err := actionConfig.Init(settings.RESTClientGetter(), namespace, os.Getenv("HELM_DRIVER"), noOpDebugLog); err != nil {
+		return nil, nil, err
+	}
+	return actionConfig, settings, nil
+}
---
+++ installutils/helminstall/installer.go
@@ -0,0 +1,208 @@
+package helminstall
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	"github.com/rotisserie/eris"
+	"github.com/solo-io/go-utils/kubeutils"
+	"helm.sh/helm/v3/pkg/chartutil"
+	"helm.sh/helm/v3/pkg/cli/values"
+	"helm.sh/helm/v3/pkg/getter"
+	"helm.sh/helm/v3/pkg/release"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"sigs.k8s.io/yaml"
+)
+
+var (
+	ReleaseAlreadyInstalledErr = func(err error, name, namespace string) error {
+		return eris.Wrapf(err, "the helm release you are trying to install (%s) appears"+
+			" to already exist in %s", name, namespace)
+	}
+)
+
+type Installer interface {
+	Install(installerConfig *InstallerConfig) error
+}
+
+type InstallerConfig struct {
+	DryRun           bool
+	CreateNamespace  bool
+	Verbose          bool
+	InstallNamespace string
+	ReleaseName      string
+	// the uri to the helm chart, can either be a local file or a valid http/https link
+	ReleaseUri  string
+	ValuesFiles []string
+	ExtraValues map[string]interface{}
+
+	PreInstallMessage  func() string
+	PostInstallMessage func(release *release.Release, err error) string
+}
+
+type installer struct {
+	helmClient   HelmClient
+	kubeNsClient NamespaceCLient
+	out          io.Writer
+}
+
+func MustInstaller() Installer {
+	cfg, err := kubeutils.GetConfig("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := kubernetes.NewForConfigOrDie(cfg)
+	return NewInstaller(DefaultHelmClient(), client.CoreV1().Namespaces(), os.Stdout)
+}
+
+func NewInstaller(helmClient HelmClient, kubeNsClient NamespaceCLient, outputWriter io.Writer) Installer {
+	return &installer{
+		helmClient:   helmClient,
+		kubeNsClient: kubeNsClient,
+		out:          outputWriter,
+	}
+}
+
+func (i *installer) Install(installerConfig *InstallerConfig) error {
+	namespace := installerConfig.InstallNamespace
+	releaseName := installerConfig.ReleaseName
+	if !installerConfig.DryRun {
+		if releaseExists, err := i.helmClient.ReleaseExists(namespace, releaseName); err != nil {
+			return err
+		} else if releaseExists {
+			return ReleaseAlreadyInstalledErr(err, releaseName, namespace)
+		}
+		if installerConfig.CreateNamespace {
+			// Create the namespace if it doesn't exist. Helm3 no longer does this.
+			i.createNamespace(namespace)
+		}
+	}
+
+	if installerConfig.PreInstallMessage != nil {
+		fmt.Fprintf(i.out, installerConfig.PreInstallMessage())
+	} else {
+		i.preInstallMessage(installerConfig)
+	}
+
+	helmInstall, helmEnv, err := i.helmClient.NewInstall(namespace, releaseName, installerConfig.DryRun)
+	if err != nil {
+		return err
+	}
+
+	if installerConfig.Verbose {
+		fmt.Printf("Looking for chart at %s\n", installerConfig.ReleaseUri)
+	}
+
+	chartObj, err := i.helmClient.DownloadChart(installerConfig.ReleaseUri)
+	if err != nil {
+		return err
+	}
+
+	// Merge values provided via the '--values' flag
+	valueOpts := &values.Options{
+		ValueFiles: installerConfig.ValuesFiles,
+	}
+	cliValues, err := valueOpts.MergeValues(getter.All(helmEnv))
+	if err != nil {
+		return err
+	}
+
+	// Merge the CLI flag values into the extra values, giving the latter higher precedence.
+	// (The first argument to CoalesceTables has higher priority)
+	completeValues := chartutil.CoalesceTables(installerConfig.ExtraValues, cliValues)
+	if installerConfig.Verbose {
+		b, err := json.Marshal(completeValues)
+		if err != nil {
+			fmt.Fprintf(i.out, "error: %v\n", err)
+		}
+		y, err := yaml.JSONToYAML(b)
+		if err != nil {
+			fmt.Fprintf(i.out, "error: %v\n", err)
+		}
+		fmt.Fprintf(i.out, "Installing the %s chart with the following value overrides:\n%s\n", chartObj.Metadata.Name, string(y))
+	}
+
+	rel, err := helmInstall.Run(chartObj, completeValues)
+	if installerConfig.PostInstallMessage != nil && !installerConfig.DryRun {
+		fmt.Fprintf(i.out, installerConfig.PostInstallMessage(rel, err))
+	}
+	if err != nil {
+		return err
+	}
+
+	if installerConfig.Verbose {
+		fmt.Printf("Successfully ran helm install with release %s\n", releaseName)
+	}
+
+	if installerConfig.DryRun {
+		fmt.Fprintf(i.out, rel.Manifest)
+	}
+
+	return nil
+}
+
+func (i *installer) createNamespace(namespace string) {
+	_, err := i.kubeNsClient.Get(namespace, metav1.GetOptions{})
+	if apierrors.IsNotFound(err) {
+		fmt.Printf("Creating namespace %s... ", namespace)
+		if _, err := i.kubeNsClient.Create(&corev1.Namespace{
+			ObjectMeta: metav1.ObjectMeta{
+				Name: namespace,
+			},
+		}); err != nil {
+			fmt.Printf("\nUnable to create namespace %s. Continuing...\n", namespace)
+		} else {
+			fmt.Printf("Done.\n")
+		}
+	} else {
+		fmt.Printf("\nUnable to check if namespace %s exists. Continuing...\n", namespace)
+	}
+
+}
+
+func (i *installer) preInstallMessage(config *InstallerConfig) {
+	if config.DryRun {
+		return
+	}
+
+	fmt.Fprintf(i.out, "starting helm installation")
+}
+
+type NamespaceCLient interface {
+	Create(ns *corev1.Namespace) (*corev1.Namespace, error)
+	Delete(name string, options *metav1.DeleteOptions) error
+	Get(name string, options metav1.GetOptions) (*corev1.Namespace, error)
+	List(opts metav1.ListOptions) (*corev1.NamespaceList, error)
+}
+
+type namespaceClient struct {
+	client v1.NamespaceInterface
+}
+
+func (n *namespaceClient) Create(ns *corev1.Namespace) (*corev1.Namespace, error) {
+	return n.client.Create(ns)
+}
+
+func (n *namespaceClient) Delete(name string, options *metav1.DeleteOptions) error {
+	return n.client.Delete(name, options)
+}
+
+func (n *namespaceClient) Get(name string, options metav1.GetOptions) (*corev1.Namespace, error) {
+	return n.client.Get(name, options)
+}
+
+func (n *namespaceClient) List(opts metav1.ListOptions) (*corev1.NamespaceList, error) {
+	return n.List(opts)
+}
+
+func NewNamespaceClient(client v1.NamespaceInterface) *namespaceClient {
+	return &namespaceClient{client: client}
+}
