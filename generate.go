aeac679e55fff1a91df72c48b71f2f6202f10d33
Harvey Xia <harvey@harveyxia.com>
2020-02-04 10:20:56 -0500

    move mock afero to its own package

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package go_utils
 
-//go:generate mockgen -package afero -destination ./testutils/mocks/afero/mock_afero_fs.go github.com/spf13/afero Fs
+//go:generate mockgen -package mock_afero -destination ./testutils/mocks/afero/mock_afero_fs.go github.com/spf13/afero Fs
