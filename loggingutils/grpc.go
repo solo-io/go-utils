7a55893c9792b1fd8ca82653b7004e8933714b12
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-02-05 11:04:50 -0500

    add WithExistingLogger (#386)
    * add WithExistingLogger
    * add grpc util and test
    * changelog
    * changelog
    * pr comment

---
+++ loggingutils/grpc.go
@@ -0,0 +1,15 @@
+package loggingutils
+
+import (
+	"context"
+	"github.com/solo-io/go-utils/contextutils"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
+)
+
+func GrpcUnaryServerLoggerInterceptor(logger *zap.SugaredLogger) grpc.UnaryServerInterceptor {
+	return func(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+		ctx = contextutils.WithExistingLogger(ctx, logger)
+		return handler(ctx, req)
+	}
+}
