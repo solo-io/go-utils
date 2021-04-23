78b0551f3b2a980b7513280ff6470b34280acb43
Scott Weiss <sdw35@cornell.edu>
2021-04-23 10:01:47 -0500

    implement error group

---
+++ errgroup/errgroup.go
@@ -0,0 +1,70 @@
+// Package errgroup provides synchronization, error propagation, and Context
+// cancelation for groups of goroutines working on subtasks of a common task.
+// Based on golangorg/x/sync
+package errgroup
+
+import (
+	"context"
+	"github.com/hashicorp/go-multierror"
+	"sync"
+)
+
+// A Group is a collection of goroutines working on subtasks that are part of
+// the same overall task.
+//
+// A zero Group is valid and does not cancel on error.
+type Group struct {
+	cancel func()
+
+	wg sync.WaitGroup
+
+	errOnce sync.Once
+	errs    error // appended with each erro
+	errLock sync.RWMutex
+}
+
+// WithContext returns a new Group and an associated Context derived from ctx.
+//
+// The derived Context is canceled the first time a function passed to Go
+// returns a non-nil error or the first time Wait returns, whichever occurs
+// first.
+func WithContext(ctx context.Context) (*Group, context.Context) {
+	ctx, cancel := context.WithCancel(ctx)
+	return &Group{cancel: cancel}, ctx
+}
+
+// Wait blocks until all function calls from the Go method have returned, then
+// returns the first non-nil error (if any) from them.
+func (g *Group) Wait() error {
+	g.wg.Wait()
+	if g.cancel != nil {
+		g.cancel()
+	}
+	g.errLock.RLock()
+	err := g.errs
+	g.errLock.RUnlock()
+	return err
+}
+
+// Go calls the given function in a new goroutine.
+//
+// The first call to return a non-nil error cancels the group; its error will be
+// returned by Wait.
+func (g *Group) Go(f func() error) {
+	g.wg.Add(1)
+
+	go func() {
+		defer g.wg.Done()
+
+		if err := f(); err != nil {
+			g.errLock.Lock()
+			g.errs = multierror.Append(g.errs, err)
+			g.errLock.Unlock()
+			g.errOnce.Do(func() {
+				if g.cancel != nil {
+					g.cancel()
+				}
+			})
+		}
+	}()
+}
