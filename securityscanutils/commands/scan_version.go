bbbd3446443a4df069c30685d7aa819f3ef463f2
Sam Heilbron <samheilbron@gmail.com>
2022-12-04 13:49:45 -0500

    simplify rseponse message

--- securityscanutils/commands/scan_version.go
+++ securityscanutils/commands/scan_version.go
@@ -3,17 +3,14 @@ package commands
 import (
 	"context"
 	"fmt"
-	"os"
-	"path"
-
-	"github.com/hashicorp/go-multierror"
-	"github.com/rotisserie/eris"
 	"github.com/solo-io/go-utils/cliutils"
 	"github.com/solo-io/go-utils/contextutils"
 	"github.com/solo-io/go-utils/osutils/executils"
 	"github.com/solo-io/go-utils/securityscanutils"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"os"
+	"path"
 )
 
 func ScanVersionCommand(ctx context.Context, rootOptions *RootOptions) *cobra.Command {
@@ -25,15 +22,12 @@ func ScanVersionCommand(ctx context.Context, rootOptions *RootOptions) *cobra.Co
 		Use:   "scan-version",
 		Short: "Run Trivy scans against images for a single version",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			results, err := doScanVersion(ctx, opts)
+			result, err := doScanVersion(ctx, opts)
 			if err != nil {
 				return err
 			}
-			if results != nil {
-				contextutils.LoggerFrom(ctx).Infof(results.Error())
-			} else {
-				contextutils.LoggerFrom(ctx).Infof("No vulnerabilities found")
-			}
+
+			contextutils.LoggerFrom(ctx).Infof(result.String())
 			return nil
 		},
 	}
@@ -60,42 +54,56 @@ func (o *scanVersionOptions) addToFlags(flags *pflag.FlagSet) {
 	cliutils.MustMarkFlagRequired(flags, "images")
 }
 
-type scanResults error
+type scanVersionResult struct {
+	OutputDir                 string
+	ImagesWithVulnerabilities []string
+}
 
-func doScanVersion(ctx context.Context, opts *scanVersionOptions) (scanResults, error) {
+func (s scanVersionResult) String() string {
+	if len(s.ImagesWithVulnerabilities) == 0 {
+		return "No vulnerabilities found"
+	}
+
+	return fmt.Sprintf("Vulernabilities found! Affected images: %v. Formatted results: %s",
+		s.ImagesWithVulnerabilities, s.OutputDir)
+}
+
+func doScanVersion(ctx context.Context, opts *scanVersionOptions) (scanVersionResult, error) {
 	contextutils.LoggerFrom(ctx).Infof("Starting ScanVersion with version=%s", opts.imageVersion)
+	versionedOutputDir := path.Join(securityscanutils.OutputScanDirectory, opts.imageVersion)
+
+	result := scanVersionResult{
+		OutputDir: versionedOutputDir,
+	}
 
 	trivyScanner := securityscanutils.NewTrivyScanner(executils.CombinedOutputWithStatus)
 
 	templateFile, err := securityscanutils.GetTemplateFile(securityscanutils.MarkdownTrivyTemplate)
 	if err != nil {
-		return nil, err
+		return result, err
 	}
 	defer func() {
 		_ = os.Remove(templateFile)
 	}()
 
-	versionedOutputDir := path.Join(securityscanutils.OutputScanDirectory, opts.imageVersion)
-	contextutils.LoggerFrom(ctx).Infof("Results will be written to %s", versionedOutputDir)
 	err = os.MkdirAll(versionedOutputDir, os.ModePerm)
 	if err != nil {
-		return nil, err
+		return result, err
 	}
 
-	var results scanResults
 	for _, imageName := range opts.imageNames {
 		image := fmt.Sprintf("%s/%s:%s", opts.imageRepository, imageName, opts.imageVersion)
 		outputFile := path.Join(versionedOutputDir, fmt.Sprintf("%s.txt", imageName))
 
 		scanCompleted, vulnerabilityFound, scanErr := trivyScanner.ScanImage(ctx, image, templateFile, outputFile)
-		contextutils.LoggerFrom(ctx).Infof(
+		contextutils.LoggerFrom(ctx).Debugf(
 			"Scanned Image: %v, ScanCompleted: %v, VulnerabilityFound: %v, Error: %v",
 			image, scanCompleted, vulnerabilityFound, scanErr)
 
 		if vulnerabilityFound {
-			results = multierror.Append(results, eris.Errorf("vulnerabilities found for %s", image))
+			result.ImagesWithVulnerabilities = append(result.ImagesWithVulnerabilities, image)
 		}
 	}
 
-	return results, nil
+	return result, nil
 }
