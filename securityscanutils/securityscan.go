182bf99fe4bc6c67c35c868823f2dc7e50a11d25
Bernie Birnbaum <bewebi@earthlink.net>
2024-09-20 12:05:25 -0400

    remove unused sarif/github upload code

--- securityscanutils/securityscan.go
+++ securityscanutils/securityscan.go
@@ -1,15 +1,9 @@
 package securityscanutils
 
 import (
-	"bytes"
-	"compress/gzip"
 	"context"
-	"encoding/base64"
-	"encoding/json"
 	"fmt"
-	"io"
 	"math"
-	"net/http"
 	"os"
 	"os/exec"
 	"path"
@@ -66,13 +60,6 @@ type SecurityScanOpts struct {
 	   │  ├─ repo2/
 	   │  │  ├─ 1.4.13/
 	   │  │  ├─ 1.5.1/
-	   ├─ sarif_results/
-	   │  ├─ repo1/
-	   │  │  ├─ 1.4.12/
-	   │  │  ├─ 1.5.0/
-	   │  ├─ repo2/
-	   │  │  ├─ 1.4.13/
-	   │  │  ├─ 1.5.1/
 	   ├─ issue_results/
 	   │  ├─ repo1/
 	   │  │  ├─ 1.4.12.md
@@ -102,10 +89,6 @@ type SecurityScanOpts struct {
 	// Required: image repo (quay.io, grc.io, gchr.io)
 	ImageRepo string
 
-	// Uploads Sarif file to github security code-scanning results
-	// e.g. https://github.com/solo-io/gloo/security/code-scanning
-	UploadCodeScanToGithub bool
-
 	// Creates github issue if image vulnerabilities are found
 	CreateGithubIssuePerVersion bool
 
@@ -120,9 +103,7 @@ type SecurityScanOpts struct {
 	AdditionalContext string
 }
 
-// Main method to call on SecurityScanner which generates .md and .sarif files
-// in OutputDir as defined above per repo. If UploadCodeScanToGithub is true,
-// sarif files will be uploaded to the repository's code-scanning endpoint.
+// GenerateSecurityScans generates .md files and writes them to the configured OutputDir for each repo
 func (s *SecurityScanner) GenerateSecurityScans(ctx context.Context) error {
 	logger := contextutils.LoggerFrom(ctx)
 
@@ -135,13 +116,8 @@ func (s *SecurityScanner) GenerateSecurityScans(ctx context.Context) error {
 	if err != nil {
 		return eris.Wrap(err, "error creating temporary markdown template file to pass to trivy")
 	}
-	sarifTplFile, err := GetTemplateFile(SarifTrivyTemplate)
-	if err != nil {
-		return eris.Wrap(err, "error creating temporary markdown template file to pass to trivy")
-	}
 	defer func() {
 		os.Remove(markdownTplFile)
-		os.Remove(sarifTplFile)
 	}()
 
 	for _, repo := range s.Repos {
@@ -158,13 +134,6 @@ func (s *SecurityScanner) GenerateSecurityScans(ctx context.Context) error {
 				return eris.Wrapf(err, "error generating markdown file from security scan for version %s", release.GetTagName())
 			}
 
-			// Only generate sarif files if we are uploading code scan results to github
-			if repo.Opts.UploadCodeScanToGithub {
-				err = repo.runGithubSarifScan(ctx, release, sarifTplFile)
-				if err != nil {
-					return eris.Wrapf(err, "error generating github sarif file from security scan for version %s", release.GetTagName())
-				}
-			}
 			logger.Debugf("Completed running markdown scan for release %s after %s", release.GetTagName(), time.Since(releaseStart).String())
 		}
 
@@ -290,46 +259,6 @@ func (r *SecurityScanRepo) RunMarkdownScan(ctx context.Context, release *github.
 	return r.issueWriter.Write(ctx, release, vulnerabilityMd)
 }
 
-func (r *SecurityScanRepo) runGithubSarifScan(ctx context.Context, release *github.RepositoryRelease, sarifTplFile string) error {
-	// We can swallow the error here, any releases with improper tag names
-	// will not be included in the filtered list
-	versionToScan, _ := semver.NewVersion(release.GetTagName())
-
-	images, err := r.GetImagesToScan(versionToScan)
-	if err != nil {
-		return err
-	}
-	version := versionToScan.String()
-	outputDir := path.Join(r.Opts.OutputDir, r.Repo, "sarif_results", version)
-	err = os.MkdirAll(outputDir, os.ModePerm)
-	if err != nil {
-		return err
-	}
-	for _, image := range images {
-		var imageWithRepo string
-		// if the image contains the repo in it (gcr.io/gloo/image-name), we don't use the Opts.ImageRepo
-		if strings.Contains(image, "/") {
-			imageWithRepo = fmt.Sprintf("%s:%s", image, version)
-		} else {
-			imageWithRepo = fmt.Sprintf("%s/%s:%s", r.Opts.ImageRepo, image, version)
-		}
-		fileName := fmt.Sprintf("%s_cve_report.sarif", image)
-		output := path.Join(outputDir, fileName)
-		success, _, err := r.trivyScanner.ScanImage(ctx, imageWithRepo, sarifTplFile, output)
-		if err != nil {
-			return eris.Wrapf(err, "error running image scan on image %s", imageWithRepo)
-		}
-		if success {
-			fmt.Printf("Security scan for version %s uploaded to github repo github.com/%s/%s\n", version, r.Owner, r.Repo)
-			err = r.UploadSecurityScanToGithub(output, version)
-			if err != nil {
-				return eris.Wrapf(err, "error uploading security scan results sarif to github for version %s", version)
-			}
-		}
-	}
-	return nil
-}
-
 func (r *SecurityScanRepo) GetImagesToScan(versionToScan *semver.Version) ([]string, error) {
 	imagesToScan := map[string]interface{}{}
 	for constraintString, images := range r.Opts.ImagesPerVersion {
@@ -350,68 +279,4 @@ func (r *SecurityScanRepo) GetImagesToScan(versionToScan *semver.Version) ([]str
 		return nil, eris.Errorf("version %s matched no constraints and has no images to scan", versionToScan.String())
 	}
 	return stringutils.Keys(imagesToScan), nil
-}
-
-type SarifMetadata struct {
-	Ref       string `json:"ref"`
-	CommitSha string `json:"commit_sha"`
-	Sarif     string `json:"sarif"`
-}
-
-// Uploads Github security scan in .sarif file format to Github Security Tab under "Code Scanning"
-func (r *SecurityScanRepo) UploadSecurityScanToGithub(fileName, versionTag string) error {
-	sha, err := githubutils.GetCommitForTag(r.Owner, r.Repo, "v"+versionTag, true)
-	if err != nil {
-		return err
-	}
-
-	githubToken, err := githubutils.GetGithubToken()
-	if err != nil {
-		return err
-	}
-	sarifFileBytes, err := os.ReadFile(fileName)
-	if err != nil {
-		return eris.Wrapf(err, "error reading sarif file %s", fileName)
-	}
-	var sarifFile bytes.Buffer
-	gzipWriter := gzip.NewWriter(&sarifFile)
-	_, err = gzipWriter.Write(sarifFileBytes)
-	if err != nil {
-		return eris.Wrap(err, "error writing gzip file")
-	}
-	gzipWriter.Close()
-	if len(sha) != 40 {
-		return eris.Errorf("Invalid SHA (%s) for version %s", sha, versionTag)
-	}
-	sarifMetadata := SarifMetadata{
-		Ref:       fmt.Sprintf("refs/tags/v%s", versionTag),
-		CommitSha: sha,
-		Sarif:     base64.StdEncoding.EncodeToString(sarifFile.Bytes()),
-	}
-
-	var sarifMetadataBuf bytes.Buffer
-	enc := json.NewEncoder(&sarifMetadataBuf)
-	err = enc.Encode(sarifMetadata)
-	if err != nil {
-		return eris.Wrap(err, "error encoding sarif metadata")
-	}
-	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/code-scanning/sarifs", r.Owner, r.Repo)
-
-	var req *http.Request
-	req, err = http.NewRequest(http.MethodPost, url, &sarifMetadataBuf)
-	req.Header.Add("Authorization", fmt.Sprintf("token %s", githubToken))
-	req.Header.Add("Content-Type", "application/json")
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil || resp.StatusCode != 200 {
-		return eris.Wrapf(err, "error uploading sarif file to github, response code: %d", resp.StatusCode)
-	}
-	defer resp.Body.Close()
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return eris.Wrapf(err, "error reading http response body from github")
-	}
-	fmt.Printf("Response from API, uploading sarif %s: \n %s\n", fileName, string(body))
-	return nil
 }
